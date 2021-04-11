3d347c3bdcdc22fc8cf75502bf2497e963418a5e
danlinja <82298205+danlinja@users.noreply.github.com>
2021-04-10 22:26:38 -0500

    Update app.js

--- apps/CleanClock/app.js
+++ apps/CleanClock/app.js
@@ -11,9 +11,11 @@ function render() {
   var time = (hour % 12 > 0 ? hour % 12: 12) + ":" + ("0" + minutes).substr(-2) + " " + (hour < 12 ? "AM" : "PM");
   g.drawString(time, (g.getWidth() - g.stringWidth(time)) / 2, (g.getHeight() - g.getFontHeight()) / 5);
   g.setColor(0, 1, 0);
-  g.fillCircle(g.getWidth() / 2, g.getHeight() * 2 / 3, 60);
-  g.setColor(0.25, 0.25, 0.25);
-  g.fillCircle(g.getWidth() / 2, g.getHeight() * 2 / 3, 56);
+  g.drawCircle(g.getWidth() / 2, g.getHeight() * 2 / 3, 60);
+  g.drawCircle(g.getWidth() / 2, g.getHeight() * 2 / 3, 59);
+  g.drawCircle(g.getWidth() / 2, g.getHeight() * 2 / 3, 58);
+  g.drawCircle(g.getWidth() / 2, g.getHeight() * 2 / 3, 57);
+  g.drawCircle(g.getWidth() / 2, g.getHeight() * 2 / 3, 56);
   g.setColor(0, 0, 1);
   var angle = Math.PI / 2 - (date.getSeconds() * 1000 + date.getMilliseconds()) / 60000 * 2 * Math.PI;
   var x = Math.cos(angle);
